eb7e84d570e32f5cf29bdde9bf36ab0778b4030a
PARK JI YOUNG <rwssss52@gmail.com>
2022-07-24 21:00:25 +0900

    박지영 : 프로젝트 최초 생성

---
+++ HollowKnight/GameEngineContents/Monster.cpp
@@ -0,0 +1,19 @@
+#include "PreCompile.h"
+#include "Monster.h"
+#include <GameEngineCore/GameEngineDefaultRenderer.h>
+
+Monster::Monster()
+{
+}
+
+Monster::~Monster()
+{
+}
+
+void Monster::Start()
+{
+	{
+		GameEngineTextureRenderer* Renderer = CreateComponent<GameEngineTextureRenderer>();
+		Renderer->GetTransform().SetLocalScale({ 100, 100, 1 });
+	}
+}
