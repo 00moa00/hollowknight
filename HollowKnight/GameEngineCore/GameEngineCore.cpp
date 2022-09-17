521ce404f8dc117c79003861879fb0d6fcc0e2aa
MOA <moa00_@naver.com>
2022-09-17 21:44:33 +0900

    박지영 : 엔진 업데이트

--- HollowKnight/GameEngineCore/GameEngineCore.cpp
+++ HollowKnight/GameEngineCore/GameEngineCore.cpp
@@ -105,7 +105,7 @@ void GameEngineCore::CoreUpdate(GameEngineCore* _UserCore)
 	}
 
 	GameEngineTime::GetInst()->Update();
-
+	GameEngineSound::Update();
 	float DeltaTime = GameEngineTime::GetDeltaTime();
 
 	GameEngineInput::GetInst()->Update(DeltaTime);
