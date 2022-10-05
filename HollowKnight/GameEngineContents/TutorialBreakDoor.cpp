e9b9faa111ccd750229dc03ecb736d0a078eea12
ji young park <moa00_@naver.com>
2022-10-06 02:44:41 +0900

    박지영 : 튜토리얼 문 버그 업데이트

--- HollowKnight/GameEngineContents/TutorialBreakDoor.cpp
+++ HollowKnight/GameEngineContents/TutorialBreakDoor.cpp
@@ -28,15 +28,21 @@ void TutorialBreakDoor::Start()
 
 void TutorialBreakDoor::Update(float _DeltaTime)
 {
-	if ((GetCollision()->IsCollision(CollisionType::CT_OBB2D, COLLISION_ORDER::Knight_Slash, CollisionType::CT_OBB2D,
+	/*if ((GetCollision()->IsCollision(CollisionType::CT_OBB2D, COLLISION_ORDER::Knight_Slash, CollisionType::CT_OBB2D,
 		std::bind(&TutorialBreakDoor::ObjectVSEffectCollision, this, std::placeholders::_1, std::placeholders::_2)) == true))
 	{
 		
-		DoorStoneParticle* DoorStoneParticle_ = GetLevel()->CreateActor<DoorStoneParticle>();
-		DoorStoneParticle_->GetTransform().SetWorldPosition({ this->GetTransform().GetWorldPosition(). x, this->GetTransform().GetWorldPosition().y + 100.f});
-		DoorStoneParticle_->SetDir(GetLevel<HollowKnightLevel>()->GetKnight()->GetKnightSlashEffect()->GetDir());
-		this->Death();
-	}
+
+	}*/
+}
+
+void TutorialBreakDoor::SetHitCollision()
+{
+	DoorStoneParticle* DoorStoneParticle_ = GetLevel()->CreateActor<DoorStoneParticle>();
+	DoorStoneParticle_->GetTransform().SetWorldPosition({ this->GetTransform().GetWorldPosition().x, this->GetTransform().GetWorldPosition().y + 100.f });
+	DoorStoneParticle_->SetDir(GetLevel<HollowKnightLevel>()->GetKnight()->GetKnightSlashEffect()->GetDir());
+	this->Death();
+
 }
 
 bool TutorialBreakDoor::ObjectVSEffectCollision(GameEngineCollision* _This, GameEngineCollision* _Other)
